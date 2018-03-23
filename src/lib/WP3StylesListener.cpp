chlorm-forks/libwpd

commit c019b27ea3235ae3c6691af623fa3895dd97e354

replace a home-grown smart ptr by the real one

src/lib/WPXTableList.cpp
@@ -27,61 +27,9 @@
 
 #include "WPXTableList.h"
 
-#include "WPXTable.h"
-
 WPXTableList::WPXTableList() :
-	m_tableList(new std::vector<WPXTable *>),
-	m_refCount(new int)
+	m_tableList(new std::vector<WPXTable *>)
 {
-	(*m_refCount) = 1;
-}
-
-WPXTableList::WPXTableList(const WPXTableList &tableList) :
-	m_tableList(tableList.get()),
-	m_refCount(tableList.getRef())
-{
-	if (m_refCount)
-		(*m_refCount)++;
-}
-
-WPXTableList &WPXTableList::operator=(const WPXTableList &tableList)
-{
-	if (this != &tableList)
-	{
-		release();
-		acquire(tableList.getRef(), tableList.get());
-	}
-
-	return (*this);
-}
-
-void WPXTableList::acquire(int *refCount, std::vector<WPXTable *> *tableList)
-{
-	m_refCount = refCount;
-	m_tableList = tableList;
-	if (m_refCount)
-		(*m_refCount)++;
-}
-
-void WPXTableList::release()
-{
-	if (m_refCount)
-	{
-		if (--(*m_refCount) == 0)
-		{
-			for (auto &iter : (*m_tableList))
-				delete iter;
-			delete m_tableList;
-			delete m_refCount;
-		}
-		m_refCount = nullptr;
-		m_tableList = nullptr;
-	}
-}
-
-WPXTableList::~WPXTableList()
-{
-	release();
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 noexpandtab: */

src/lib/WPXTableList.h
@@ -27,6 +27,7 @@
 #ifndef _WPXTABLELIST_H
 #define _WPXTABLELIST_H
 
+#include <memory>
 #include <vector>
 
 class WPXTable;
@@ -35,9 +36,6 @@ class WPXTableList
 {
 public:
 	WPXTableList();
-	WPXTableList(const WPXTableList &);
-	WPXTableList &operator=(const WPXTableList &tableList);
-	virtual ~WPXTableList();
 
 	WPXTable *operator[](unsigned long i)
 	{
@@ -49,19 +47,7 @@ public:
 	}
 
 private:
-	void release();
-	void acquire(int *refCount, std::vector<WPXTable *> *tableList);
-	int *getRef() const
-	{
-		return m_refCount;
-	}
-	std::vector<WPXTable *> *get() const
-	{
-		return m_tableList;
-	}
-
-	std::vector<WPXTable *> *m_tableList;
-	int *m_refCount;
+	std::shared_ptr<std::vector<WPXTable *>> m_tableList;
 };
 
 #endif /* _WPXTABLELIST_H */

commit dfca3b5a137756930a57ba62ef4b4aee552f305d

ofz#7062 do not leak tables
Regression from commit c019b27ea3235ae3c6691af623fa3895dd97e354 .

src/lib/WP3StylesListener.cpp
@@ -246,7 +246,7 @@ void WP3StylesListener::startTable()
 	if (!isUndoOn())
 	{
 		m_currentPageHasContent = true;
-		m_currentTable = new WPXTable();
+		m_currentTable = std::make_shared<WPXTable>();
 		m_tableList.add(m_currentTable);
 	}
 }
@@ -260,7 +260,7 @@ void WP3StylesListener::_handleSubDocument(const WPXSubDocument *subDocument, WP
 	if (subDocumentType == WPX_SUBDOCUMENT_HEADER_FOOTER)
 	{
 		bool oldCurrentPageHasContent = m_currentPageHasContent;
-		WPXTable *oldCurrentTable = m_currentTable;
+		auto oldCurrentTable = m_currentTable;
 		WPXTableList oldTableList = m_tableList;
 		m_tableList = tableList;
 

src/lib/WP3StylesListener.h
@@ -115,7 +115,7 @@ private:
 	WPXPageSpan m_currentPage;
 
 	WPXTableList m_tableList;
-	WPXTable *m_currentTable;
+	std::shared_ptr<WPXTable> m_currentTable;
 	double m_tempMarginLeft, m_tempMarginRight;
 	bool m_currentPageHasContent;
 	bool m_isSubDocument;

src/lib/WP5StylesListener.cpp
@@ -261,7 +261,7 @@ void WP5StylesListener::startTable()
 	if (!isUndoOn())
 	{
 		m_currentPageHasContent = true;
-		m_currentTable = new WPXTable();
+		m_currentTable = std::make_shared<WPXTable>();
 		m_tableList.add(m_currentTable);
 	}
 }
@@ -303,7 +303,7 @@ void WP5StylesListener::_handleSubDocument(const WPXSubDocument *subDocument, WP
 		if (subDocumentType == WPX_SUBDOCUMENT_HEADER_FOOTER)
 		{
 			bool oldCurrentPageHasContent = m_currentPageHasContent;
-			WPXTable *oldCurrentTable = m_currentTable;
+			auto oldCurrentTable = m_currentTable;
 			WPXTableList oldTableList = m_tableList;
 			m_tableList = tableList;
 

src/lib/WP5StylesListener.h
@@ -101,7 +101,7 @@ private:
 	WPXPageSpan m_currentPage, m_nextPage;
 
 	WPXTableList m_tableList;
-	WPXTable *m_currentTable;
+	std::shared_ptr<WPXTable> m_currentTable;
 	double m_tempMarginLeft, m_tempMarginRight;
 	bool m_currentPageHasContent;
 	bool m_isSubDocument;

src/lib/WP6ContentListener.cpp
@@ -118,7 +118,7 @@ WP6ContentParsingState::WP6ContentParsingState(WPXTableList tableList, unsigned 
 	m_isListReference(false),
 
 	m_tableList(tableList),
-	m_currentTable(nullptr),
+	m_currentTable(),
 
 	m_nextTableIndice(nextTableIndice),
 

src/lib/WP6ContentListener.h
@@ -101,7 +101,7 @@ struct WP6ContentParsingState
 	bool m_isListReference;
 
 	WPXTableList m_tableList;
-	WPXTable *m_currentTable;
+	std::shared_ptr<WPXTable> m_currentTable;
 	unsigned m_nextTableIndice;
 
 	std::stack<unsigned> m_listLevelStack;

src/lib/WP6StylesListener.cpp
@@ -267,7 +267,7 @@ void WP6StylesListener::defineTable(const unsigned char /* position */, const un
 	if (!isUndoOn())
 	{
 		m_currentPageHasContent = true;
-		m_currentTable = new WPXTable();
+		m_currentTable = std::make_shared<WPXTable>();
 		m_tableList.add(m_currentTable);
 		m_isTableDefined = true;
 	}
@@ -278,7 +278,7 @@ void WP6StylesListener::startTable()
 	if (!isUndoOn() && !m_isTableDefined)
 	{
 		m_currentPageHasContent = true;
-		m_currentTable = new WPXTable();
+		m_currentTable = std::make_shared<WPXTable>();
 		m_tableList.add(m_currentTable);
 		m_isTableDefined = false;
 	}
@@ -355,7 +355,7 @@ void WP6StylesListener::_handleSubDocument(const WPXSubDocument *subDocument, WP
 		m_subDocuments.insert(subDocument);
 		bool oldIsSubDocument = m_isSubDocument;
 		m_isSubDocument = true;
-		WPXTable *oldCurrentTable = m_currentTable;
+		auto oldCurrentTable = m_currentTable;
 		if (subDocumentType == WPX_SUBDOCUMENT_HEADER_FOOTER)
 		{
 			bool oldCurrentPageHasContent = m_currentPageHasContent;

src/lib/WP6StylesListener.h
@@ -151,7 +151,7 @@ private:
 	WPXPageSpan m_currentPage;
 
 	WPXTableList m_tableList;
-	WPXTable *m_currentTable;
+	std::shared_ptr<WPXTable> m_currentTable;
 	double m_tempMarginLeft, m_tempMarginRight;
 	bool m_currentPageHasContent;
 	bool m_isTableDefined;

src/lib/WPXTableList.cpp
@@ -28,7 +28,7 @@
 #include "WPXTableList.h"
 
 WPXTableList::WPXTableList() :
-	m_tableList(new std::vector<WPXTable *>)
+	m_tableList(new std::vector<std::shared_ptr<WPXTable>>)
 {
 }
 

src/lib/WPXTableList.h
@@ -37,17 +37,17 @@ class WPXTableList
 public:
 	WPXTableList();
 
-	WPXTable *operator[](unsigned long i)
+	std::shared_ptr<WPXTable> &operator[](unsigned long i) const
 	{
 		return (*m_tableList)[i];
 	}
-	void add(WPXTable *table)
+	void add(const std::shared_ptr<WPXTable> &table)
 	{
 		m_tableList->push_back(table);
 	}
 
 private:
-	std::shared_ptr<std::vector<WPXTable *>> m_tableList;
+	std::shared_ptr<std::vector<std::shared_ptr<WPXTable>>> m_tableList;
 };
 
 #endif /* _WPXTABLELIST_H */